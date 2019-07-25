b3050eca92850cbd58ac28037bcbd064fcb65cdf
Barry Harris <brwnrclse@gmail.com>
2019-07-24 23:05:38 -0400

    Remove deprecated settings

--- linter.py
+++ linter.py
@@ -16,12 +16,7 @@ from SublimeLinter.lint import Linter, util
 class Tailor(Linter):
     """Provides an interface to tailor."""
 
-    syntax = 'swift'
     cmd = 'tailor'
-    executable = None
-    version_args = '--version'
-    version_re = r'(?P<version>\d+\.\d+\.\d+)'
-    version_requirement = '>= 0.9.0'
     config_file = ('-c', '.tailor.yml', '~')
     regex = (
         r'^.+?:(?P<line>\d+):(?P<col>\d+): '
@@ -29,12 +24,12 @@ class Tailor(Linter):
         r'(?P<message>.+)'
     )
     multiline = False
-    line_col_base = (1, 1)
     tempfile_suffix = 'swift'
     error_stream = util.STREAM_BOTH
     selectors = {}
-    word_re = None
-    defaults = {}
-    inline_settings = None
-    inline_overrides = None
-    comment_re = None
+    defaults = {
+        'selector': 'source.swift',
+        '--ignore=,': '',
+        '--warn=,': '',
+        '--error=,': ''
+    }
